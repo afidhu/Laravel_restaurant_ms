0edf9d49736fed6e9adf03cb82437cf8c75eb996
afidhu <afidhumatata81@gmail.com>
2025-11-24 06:39:52 -0700

    Fix UI

--- resources/views/pages/Home.blade.php
+++ resources/views/pages/Home.blade.php
@@ -271,7 +271,7 @@
     </div>
 </footer>
 
-<<!-- jQuery -->
+<!-- jQuery -->
 <script src="assets/js/jquery-2.1.0.min.js"></script>
 <script src="assets/js/popper.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
